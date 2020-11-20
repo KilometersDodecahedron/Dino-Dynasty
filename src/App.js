af9f8bfe4489b89f4e5ecae0306e4ae2fc61570a
Troy Berentsen <troyberentsen94@gmail.com>
2020-11-19 20:39:45 -0500

    Added styling for webpage and CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import Platformer from "./games/PlatformerComponent.js"
-import './App.css';
+import React from "react";
+import Platformer from "./games/PlatformerComponent.js";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="Dino Dynasty 
     A Race Before Time">
+      <Header />
       <Platformer />
+      <Footer />
     </div>
   );
 }
