58034137585a19d5e57b8fae615c7b717f671419
Miles Cohn <milesselim@icloud.com>
2020-11-24 20:50:43 -0500

    Updates

---
+++ platformer/assets/js/collectables/ColorChangerPickup.js
@@ -0,0 +1,22 @@
+export default class ColorChangerPickup extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        this.color = "green";
+        //given in the child
+        this.pngKey;
+        this.scale = 0.5;
+        this.radius = 16;
+    }
+
+    callbackFunction(){
+        this.body.setAllowGravity(false);
+        this.setScale(this.scale, this.scale);
+        this.setCircle(this.radius);
+        this.setTexture(this.pngKey);
+    }
+
+    collect(){
+        this.destroy();
+    }
+}
