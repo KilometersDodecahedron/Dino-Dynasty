5e8f62f3a1bb03f8f1d31e60331bcc23ab504398
Miles Cohn <milesselim@icloud.com>
2020-12-05 19:27:30 -0500

    Commented out the router for now, as it seems to be messing with the game path

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import Forget from "./components/Forget";
-import Account from "./components/Account";
+// import Account from "./components/Account";
 import Reset from "./components/Reset";
 import Welcome from "./components/Welcome"
 
@@ -14,16 +14,17 @@ function App() {
   return (
     <div className="Dino Dynasty 
     A Race Before Time">
-      <Router>
+      {/* <Router>
         <div>
           <Route exact path="/" component={Welcome} />
-          {/* <Route exact path="/Forget" component={Forget} />*/}
+          <Route exact path="/Forget" component={Forget} />
           <Route exact path="/Account" component={Account} />
           <Route exact path="/Reset" component={Reset} />
           <Route exact path="/Login" component={Login} />
           <Route exact path="/Game" component={Platformer} />
         </div>
-      </Router>
+      </Router> */}
+      <Platformer />
     </div>
   );
 }
